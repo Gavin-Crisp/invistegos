109949fdd52a6f190149a5a1fd4533a539400b6e
Gavin-Crisp <gavin@gavincrisp.com.au>
2026-03-01 16:02:50 +1030

    Added CscMatrix

--- shared/ldpc/matrix.zig
+++ shared/ldpc/matrix.zig
@@ -2,27 +2,35 @@ const std = @import("std");
 const config = @import("config");
 const ldpc = @import("../ldpc.zig");
 
-pub const CodeMatrix = Template(config.c_nodes, ldpc.v_nodes);
-pub const GeneratorMatrix = Template(config.d_nodes, config.c_nodes);
+pub const CodeMatrix = SimpleMatrix(config.c_nodes, ldpc.v_nodes);
+pub const GeneratorMatrix = SimpleMatrix(config.d_nodes, config.c_nodes);
 
-fn Template(n: comptime_int, m: comptime_int) type {
+fn SimpleMatrix(m: comptime_int, n: comptime_int) type {
+    const Row = @Type(.{ .int = .{ .signedness = .unsigned, .bits = std.math.log2_int_ceil(comptime_int, n) } });
+    const Col = @Type(.{ .int = .{ .signedness = .unsigned, .bits = std.math.log2_int_ceil(comptime_int, m) } });
+
     return struct {
-        buf: [n * m]u1 = [_]u1{0} ** (n * m),
+        buf: [m * n]u1 = [_]u1{0} ** (m * n),
 
         const Self = @This();
 
-        pub const Row = @Type(.{ .int = .{ .signedness = .unsigned, .bits = std.math.log2_int_ceil(comptime_int, n) } });
-        pub const Col = @Type(.{ .int = .{ .signedness = .unsigned, .bits = std.math.log2_int_ceil(comptime_int, m) } });
-
         pub fn get(self: Self, row: Row, col: Col) *u1 {
+            std.debug.assert(row < m);
+            std.debug.assert(col < n);
+
             return &self.buf[row * m + col];
         }
 
         pub fn get_row(self: Self, row: Row) []u1 {
+            std.debug.assert(row < m);
+
             self.buf[row * m .. (row + 1) * m];
         }
 
         pub fn add_rows(self: Self, from: Row, to: Row) void {
+            std.debug.assert(from < m);
+            std.debug.assert(to < m);
+
             const from_row = self.get_row(from);
             const to_row = self.get_row(to);
 
@@ -32,6 +40,9 @@ fn Template(n: comptime_int, m: comptime_int) type {
         }
 
         pub fn swap_rows(self: Self, row1: Row, row2: Row) void {
+            std.debug.assert(row1 < m);
+            std.debug.assert(row2 < m);
+
             if (row1 == row2)
                 return;
 
@@ -43,6 +54,55 @@ fn Template(n: comptime_int, m: comptime_int) type {
                 r1_el.* = r2_el.*;
                 r2_el.* = tmp;
             }
+        }
+    };
+}
+
+pub fn CscMatrix(num_nonzeroes: comptime_int, m: comptime_int, n: comptime_int) type {
+    const RowIndices = @Type(.{ .array = .{ .len = num_nonzeroes, .child = usize } });
+    const ColumnIndices = @Type(.{ .array = .{ .len = n + 1, .child = usize } });
+
+    return struct {
+        row_indices: RowIndices,
+        column_indices: ColumnIndices,
+
+        const Self = @This();
+
+        pub fn get_elem(self: Self, row: usize, col: usize) u1 {
+            std.debug.assert(row < m);
+            std.debug.assert(col < n);
+
+            const col_start = self.column_indices[col];
+            const col_end = self.column_indices[col + 1];
+            const elem_rows = self.row_indices[col_start..col_end];
+
+            for (elem_rows) |elem| {
+                if (row == elem) {
+                    return 1;
+                }
+            }
+
+            return 0;
+        }
+
+        pub fn get_col(self: Self, col: usize) [m]u1 {
+            std.debug.assert(col < n);
+
+            const col_start = self.column_indices[col];
+            const col_end = self.column_indices[col + 1];
+            const elem_rows = self.row_indices[col_start..col_end];
+
+            var out: [m]u1 = .{0};
+            var elem_index = 0;
+
+            for (0..m) |row| {
+                if (row == elem_rows[elem_index]) {
+                    out[row] = 1;
+                    elem_index += 1;
+                }
+            }
+
+            return out;
         }
     };
 }
