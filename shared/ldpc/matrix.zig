d85acef116be57713948f169837090c7082d0d10
Gavin-Crisp <gavin@gavincrisp.com.au>
2026-03-01 16:02:50 +1030

    Added SimpleMatrix.into_csc

--- shared/ldpc/matrix.zig
+++ shared/ldpc/matrix.zig
@@ -10,26 +10,28 @@ fn SimpleMatrix(m: comptime_int, n: comptime_int) type {
     const Col = @Type(.{ .int = .{ .signedness = .unsigned, .bits = std.math.log2_int_ceil(comptime_int, m) } });
 
     return struct {
-        buf: [m * n]u1 = [_]u1{0} ** (m * n),
+        buf: [M * N]u1 = [_]u1{0} ** (M * N),
 
         const Self = @This();
+        const M = m;
+        const N = n;
 
         pub fn get(self: Self, row: Row, col: Col) *u1 {
-            std.debug.assert(row < m);
-            std.debug.assert(col < n);
+            std.debug.assert(row < M);
+            std.debug.assert(col < N);
 
-            return &self.buf[row * m + col];
+            return &self.buf[row * M + col];
         }
 
         pub fn get_row(self: Self, row: Row) []u1 {
-            std.debug.assert(row < m);
+            std.debug.assert(row < M);
 
-            self.buf[row * m .. (row + 1) * m];
+            self.buf[row * M .. (row + 1) * M];
         }
 
         pub fn add_rows(self: Self, from: Row, to: Row) void {
-            std.debug.assert(from < m);
-            std.debug.assert(to < m);
+            std.debug.assert(from < M);
+            std.debug.assert(to < M);
 
             const from_row = self.get_row(from);
             const to_row = self.get_row(to);
@@ -40,8 +42,8 @@ fn SimpleMatrix(m: comptime_int, n: comptime_int) type {
         }
 
         pub fn swap_rows(self: Self, row1: Row, row2: Row) void {
-            std.debug.assert(row1 < m);
-            std.debug.assert(row2 < m);
+            std.debug.assert(row1 < M);
+            std.debug.assert(row2 < M);
 
             if (row1 == row2)
                 return;
@@ -55,6 +57,38 @@ fn SimpleMatrix(m: comptime_int, n: comptime_int) type {
                 r2_el.* = tmp;
             }
         }
+
+        pub fn into_csc(self: Self) Csc(self) {
+            var out = .{};
+
+            out.column_indices[0] = 0;
+            var elem_idx = 0;
+
+            for (0..N) |col_idx| {
+                for (0..M) |row_idx| {
+                    if (self.get(row_idx, col_idx) == 1) {
+                        out.row_indices[elem_idx] = row_idx;
+                        elem_idx += 1;
+                    }
+                }
+
+                out.column_indices[col_idx + 1] = elem_idx;
+            }
+
+            return out;
+        }
+
+        fn Csc(self: Self) type {
+            var num_nonzeroes = 0;
+
+            for (self.buf) |elem| {
+                if (elem == 1) {
+                    num_nonzeroes += 1;
+                }
+            }
+
+            return CscMatrix(num_nonzeroes, M, N);
+        }
     };
 }
 
@@ -67,10 +101,12 @@ pub fn CscMatrix(num_nonzeroes: comptime_int, m: comptime_int, n: comptime_int) 
         column_indices: ColumnIndices,
 
         const Self = @This();
+        const M = m;
+        const N = n;
 
-        pub fn get_elem(self: Self, row: usize, col: usize) u1 {
-            std.debug.assert(row < m);
-            std.debug.assert(col < n);
+        pub fn get(self: Self, row: usize, col: usize) u1 {
+            std.debug.assert(row < M);
+            std.debug.assert(col < N);
 
             const col_start = self.column_indices[col];
             const col_end = self.column_indices[col + 1];
@@ -85,17 +121,17 @@ pub fn CscMatrix(num_nonzeroes: comptime_int, m: comptime_int, n: comptime_int) 
             return 0;
         }
 
-        pub fn get_col(self: Self, col: usize) [m]u1 {
-            std.debug.assert(col < n);
+        pub fn get_col(self: Self, col: usize) [M]u1 {
+            std.debug.assert(col < N);
 
             const col_start = self.column_indices[col];
             const col_end = self.column_indices[col + 1];
             const elem_rows = self.row_indices[col_start..col_end];
 
-            var out: [m]u1 = .{0};
+            var out: [M]u1 = .{0};
             var elem_index = 0;
 
-            for (0..m) |row| {
+            for (0..M) |row| {
                 if (row == elem_rows[elem_index]) {
                     out[row] = 1;
                     elem_index += 1;
