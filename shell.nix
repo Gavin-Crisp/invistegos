8b414d1631a0205f70270fdee1cccf6c8fc5ad2a
Gavin Crisp <gavin@gavincrisp.com.au>
2026-04-25 23:00:07 +0930

    shell.nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,12 @@
 let
   pkgs = import <nixpkgs> { };
-  unstableTarball = fetchTarball
-      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
-  unstable = import unstableTarball { };
 in pkgs.mkShell {
-  packages = [
-    pkgs.upx
-	pkgs.git
-	pkgs.vim
-	pkgs.openssh_gssapi
-	pkgs.less
-	unstable.zig
+  packages = with pkgs; [
+    upx
+	git
+	vim
+	openssh_gssapi
+	less
+	zig
   ];
 }
