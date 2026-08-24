c0665ab7746b7052695113d3c31aafb6494e03e4
Gavin Crisp <gavin@gavincrisp.com.au>
2026-08-24 18:26:34 +0930

    Improve triggerFlush

--- kernmod/src/InvistegosContext.zig
+++ kernmod/src/InvistegosContext.zig
@@ -35,8 +35,9 @@ pub fn destroy(self: *Self, ti: *linux.DmTarget) void {
     self.alloc.destroy(self);
 }
 
-pub fn triggerFlush(self: *Self) !void {
+pub fn triggerFlush(self: Self) !void {
     // # Flush underlying
+    // FIXME: GFP flags
     const bio = linux.bioAlloc(self.dev.bdev.?, 0, @intFromEnum(linux.ReqOp.flush), 0) orelse return error.OOM;
     // # Flush cache
     // TODO
